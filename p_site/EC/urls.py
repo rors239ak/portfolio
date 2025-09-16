0deac4b1cfb54b5c74d5569d7ebcfe5d5d0e63ae
rors239ak <rors239ak@gmail.com>
2025-09-16 10:15:41 +0900

    カテゴリー追加

--- p_site/EC/urls.py
+++ p_site/EC/urls.py
@@ -6,7 +6,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('product_list/', views.product_list, name='product_list'),
+    path('products/', views.product_list, name='product_list'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
+    path('products/new/', views.create_product, name='product_create'),  # 追加
 ]
--- p_site/EC/views.py
+++ p_site/EC/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
 from django.db.models import Q
 from .models import Product
+from .forms import ProductForm
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth.decorators import login_required
+from django.urls import path
+from . import views
 
 #トップページ
 @login_required
@@ -23,6 +26,18 @@ def product_list(request):
     products = Product.objects.all()
     return render(request, 'EC/product_list.html', {"products": products})
 
+#商品作成ページ
+@login_required
+def create_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+    else:
+        form = ProductForm()
+    return render(request, 'EC/product_create.html', {'form': form})
+
 #会員登録ビュー
 def signup(request):
     if request.method == 'POST':
@@ -34,6 +49,7 @@ def signup(request):
         form = UserCreationForm()
     return render(request, 'EC/signup.html', {'form': form})
 
+# ログインビュー
 def login(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
@@ -43,3 +59,5 @@ def login(request):
     else:
         form = AuthenticationForm()
     return render(request, 'EC/login.html', {'form': form})
+
+
